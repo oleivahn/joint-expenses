989323f562ca732ffabe22470c2edf01abfce2df
Omar Leiva <oleivahn@gmail.com>
2022-05-16 01:06:10 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* <p>My App</p> */}
+      <div className="App">
+        <header className="App-header">
+          <p>Welcome to my bare application</p>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
